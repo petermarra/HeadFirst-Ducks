bd405eaa68813615ce728d49079ee4e916cb0760
Peter Marra <neverquit@roadrunner.com>
2020-09-16 20:53:10 -0400

    Add Duck class Add KindofDuck  enum

---
+++ Ducks/Duck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ducks
+{
+    class Duck
+    {
+        public int size;
+        public KindOfDuck Kind;
+    }
+}
